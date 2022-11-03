5cc9957dba329dae6a100127d357d1b3866101d5
Lordfirespeed <joe@joeclack.me.uk>
2022-11-03 16:47:33 +0000

    add meta tags to navbar HTML add stack overflow reference to navbar JS

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,3 +1,4 @@
+// https://stackoverflow.com/a/55081177/11045433
 
 fetch("/progblack_formative_1/assets/html/navbar.html")
     .then(stream => stream.text())
@@ -7,7 +8,7 @@ function define(html) {
     class NavBar extends HTMLElement {
         on_document_loaded() {
             let active_name = this.getAttribute("current-active");
-            let nav_link_element = document.getElementById("navbar-nav-link-" + active_name);
+            let nav_link_element = this.getElementById("navbar-nav-link-" + active_name);
             if (nav_link_element === null) { return }
 
             nav_link_element.classList.add("active");
@@ -19,7 +20,7 @@ function define(html) {
 
             this.innerHTML = html;
 
-            if (document.readyState === "loading") {
+            if (this.readyState === "loading") {
                 addEventListener("DOMContentLoaded", this.on_document_loaded)
             }
             else {
